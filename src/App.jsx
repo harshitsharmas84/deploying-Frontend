c05f5a84f100715d07a213e869f903b6454bf9fe
harshitsharmas84 <harshit.sharma.s84@kalvium.community>
2025-03-07 10:31:03 +0530

    Done

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <div>
+        <h1>Deploying-Frontend</h1>
+        <h2>Harshit Sharma</h2>
+        <h3>harshit.sharma.s84@kalivum.community</h3>
+      </div>
+    </>
+  );
+}
+
+export default App;
